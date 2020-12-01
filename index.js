5dd921c36579cdbb4aa572a8d1ef34f100ba8767
gutnar <gutnar@gmail.com>
2020-12-01 12:22:03 +0100

    Add module main script

---
+++ index.js
@@ -0,0 +1,8 @@
+module.exports = {
+    areas: require('./areas.json'),
+    heroes: require('./heroes.json'),
+    itemImages: require('./item-images.json'),
+    itemSlots: require('./item-slots.json'),
+    quests: require('./quests.json'),
+    stats: require('./stats.json')
+};
