deeb46e1b964649b3d2c3ae6ad82c343d75e1708
Jorge <holyone.pt@gmail.com>
2021-07-03 17:32:04 +0100

    Removed empty entry for FxR. Added items.json

--- index.d.ts
+++ index.d.ts
@@ -1,111 +1,127 @@
 declare module '@diablorun/diablorun-data' {
   export const areas: {
     [id: string]: {
-      act: number
-      name: string
-      town: boolean
-    }
-  }
+      act: number;
+      name: string;
+      town: boolean;
+    };
+  };
 
   export const heroes: {
-    id: string
-    name: string
-  }[]
+    id: string;
+    name: string;
+  }[];
 
   export const itemImages: {
-    [baseName: string]: string
-  }
+    [baseName: string]: string;
+  };
 
-  export const itemSlots: string[]
+  export const itemSlots: string[];
 
   export const quests: {
     [id: string]: {
-      act: number
-      order: number
-      boss: boolean
-      name: string
-      short_name: string
-    }
-  }
+      act: number;
+      order: number;
+      boss: boolean;
+      name: string;
+      short_name: string;
+    };
+  };
 
   export const stats: {
-    [stat: string]: string
-  }
+    [stat: string]: string;
+  };
 
   export const hirelings: {
-    [id: string]: string
-  }
+    [id: string]: string;
+  };
 
   export const skills: {
-    [id: string]: string
-  }
+    [id: string]: string;
+  };
 
-  export const levelExperience: number[]
+  export const levelExperience: number[];
 
-  export const difficulties: string[]
+  export const difficulties: string[];
 
   export const runewords: [
     {
-      allowedItems: string
-      isLadder: boolean
-      name: string
-      properties: string[]
-      runeword: string
-      runes: string[]
+      allowedItems: string;
+      isLadder: boolean;
+      name: string;
+      properties: string[];
+      runeword: string;
+      runes: string[];
     },
-  ]
+  ];
 
   export const recipes: {
-    [name: string]: string
-  }
+    [name: string]: string;
+  };
 
   export const fbr: {
-    [name: string]: string
-  }
+    [name: string]: string;
+  };
 
   export const fcr: {
-    [name: string]: string
-  }
+    [name: string]: string;
+  };
 
   export const fhr: {
-    [name: string]: string
-  }
+    [name: string]: string;
+  };
 
   export const acronyms: {
-    [name: string]: string
-  }
+    [name: string]: string;
+  };
 
   export const unids: {
-    [name: string]: string[]
-  }
+    [name: string]: string[];
+  };
 
-  export const uniques: {
-    name: string
-    unidItem: string
-    cLevel: number
-    requirements: string[]
-    properties: string[]
-  }
+  export const uniques: [
+    {
+      name: string;
+      unidItem: string;
+      cLevel: number;
+      requirements: string[];
+      properties: string[];
+    },
+  ];
 
   export const setItems: [
     {
-      name: string
-      unidItem: string
-      cLevel: number
-      requirements: string[]
-      properties: string[]
-      setName: string
+      name: string;
+      unidItem: string;
+      cLevel: number;
+      requirements: string[];
+      properties: string[];
+      setName: string;
     },
-  ]
+  ];
 
-  export const sets: {
-    name: string
-    additionalTrigger: string[]
-    details: string[]
-    items: string[]
-  }
+  export const sets: [
+    {
+      name: string;
+      additionalTrigger: string[];
+      details: string[];
+      items: string[];
+    },
+  ];
 
   export const countries: {
-    [country: string]: string
-  }
+    [country: string]: string;
+  };
+
+  export const items: [
+    {
+      properties: string[];
+      baseItem: string;
+      cLevel?: number;
+      is1h?: boolean;
+      is2h?: boolean;
+      isThrow?: boolean;
+      paladinShield?: boolean;
+    },
+  ];
 }
